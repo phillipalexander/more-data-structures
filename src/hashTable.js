fa4c940dfa8f2cb001f942d1ddab9fb85f2275ca
Hack Reactor Students <git@phillipalexander.io>
2013-06-21 15:29:52 -0700

    add notes on hash table functions to src/hashTable.js

--- src/hashTable.js
+++ src/hashTable.js
@@ -22,3 +22,32 @@ HashTable.prototype.remove = function(){
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you
+// var getIndexBelowMaxForKey = function(str, max){
+
+// Phillip's
+
+var getHashFromKey = function (key) {
+  var hashOfKey;
+  // some 'code'
+  return hashOfKey;
+};
+
+var getKeyValuePairsThatMatchHashedKey = function (hashedKey) {
+  var KeyValuePairsThatMatchHashedKey;
+  // some 'code'
+  return KeyValuePairsThatMatchHashedKey;
+};
+
+var lookupValueFromKey = function (key) {
+  var hashedKey = getHashFromKey(key);
+  var keyValuePairs = getKeyValuePairsThatMatchHashedKey(hashedKey);
+  if (keyValuePairs > 1) {
+    for (var i = 0; i < keyValuePairs.length; i++) {
+      if (keyValuePairs[i].key === key) {
+        return keyValuePairs[i].value;
+      }
+    }
+  } else {
+    return keyValuePairs.value;
+  }
+};
