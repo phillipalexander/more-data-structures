7a9edc572390843cd557d5f56e59d0c2ae91ddbb
Hack Reactor Students <git@phillipalexander.io>
2013-06-20 20:10:56 -0700

    Add spec to test adding nodes to linked list. Add addToTail to LinkedList

--- spec/linkedListSpec.js
+++ spec/linkedListSpec.js
@@ -16,5 +16,17 @@ describe("linkedList", function() {
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
 
+  it("should return a boolean if the list contains a value", function() {
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    linkedList.addToTail(3);
+    debugger;
+    expect(linkedList.contains(3)).toEqual(true);
+    expect(linkedList.contains(4)).toEqual(false);
+    //expect(linkedList.contains).toEqual(jasmine.any(Function));
+  });
+
+
+
   // add more tests here to test the functionality of linkedList
 });
--- src/linkedList.js
+++ src/linkedList.js
@@ -4,13 +4,23 @@ var makeLinkedList = function(){
   newLinkedList.head = null;
   newLinkedList.tail = null;
 
-  newLinkedList.addToTail = function(){
+  newLinkedList.addToTail = function(value){
+
+    var node = makeNode(value);
+
+    if(this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   newLinkedList.removeHead = function(){
   };
 
-  newLinkedList.contains = function(){
+  newLinkedList.contains = function(value){
   };
 
   return newLinkedList;
@@ -26,3 +36,12 @@ var makeNode = function(value){
 
   return newNode;
 };
+
+/*
+A linkedList class, in functional style, with the following properties:
+  .head property, a linkedListNode instance
+  .tail property, a linkedListNode instance
+  .addToTail() method, takes a value and adds it to the end of the list
+  .removeHead() method, removes the first node from the list and returns its value
+  .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
+*/
