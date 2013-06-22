ab4654b215e06f9d1adce80ccb7846c8941dacfd
Hack Reactor Students <git@phillipalexander.io>
2013-06-22 11:44:08 -0700

    add setMethods.getIndex()

--- src/set.js
+++ src/set.js
@@ -1,18 +1,24 @@
-var makeSet = function(){
+var makeSet = function() {
   var set = Object.create(makeSet.setMethods);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 makeSet.setMethods = {
 
-  add : function(){
-  },
+  add: function() {},
 
-  contains : function(){
-  },
+  contains: function() {},
 
-  remove : function(){
+  remove: function() {},
+
+  getIndex: function(value) {
+    for (var i = 0; i < this._storage.length; i++) {
+      if (this._storage[i] === value) {
+        return i;
+      }
+    }
+    return -1;
   }
 
 };
