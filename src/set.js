f85911ceaeaf2b1d49628a7ce8b553618cf8d2f0
Hack Reactor Students <git@phillipalexander.io>
2013-06-22 16:52:01 -0700

    refactor set to use object instead of array

--- src/set.js
+++ src/set.js
@@ -1,43 +1,45 @@
 var makeSet = function() {
   var set = Object.create(makeSet.setMethods);
-  set._storage = [];
+  //set._storage = [];
+  set._storage = {};
   return set;
 };
 
 makeSet.setMethods = {
 
-  add: function(value) {
+  // add: function(value) {
+  //   value += '';
+  //   if (this.contains(value) === false) {
+  //     this._storage.push(value);
+  //   }
+  //   return this._storage;
+  // },
+
+  add : function(value) {
     value += '';
-    if (this.contains(value) === false) {
-      this._storage.push(value);
-    }
+    this._storage[value] = true;
     return this._storage;
   },
 
   contains: function(value) {
     value += '';
-    if (this._getIndex(value) < 0) {
-      return false;
-    }
-    return true;
+    return this._storage[value] || false;
   },
 
   remove: function(value) {
     value += '';
-    var index = this._getIndex(value);
-    if (index > -1) {
-      this._storage.splice(index, 1);
-    }
+    delete this._storage[value];
     return this._storage;
-  },
-
-  _getIndex: function(value) {
-    for (var i = 0; i < this._storage.length; i++) {
-      if (this._storage[i] === value) {
-        return i;
-      }
-    }
-    return -1;
   }
+
+
+  // remove: function(value) {
+  //   value += '';
+  //   var index = this._getIndex(value);
+  //   if (index > -1) {
+  //     this._storage.splice(index, 1);
+  //   }
+  //   return this._storage;
+  // },
 
 };
